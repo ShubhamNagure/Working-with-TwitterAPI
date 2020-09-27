d74b48447fccadc002f9d0fb5d16ba954b028e96
Shubham Nagure <49149160+ShubhamNagure@users.noreply.github.com>
2020-09-28 01:52:36 +0530

    Update TwitAPIWork1.py

--- TwitAPIWork1.py
+++ TwitAPIWork1.py
@@ -1,7 +1,7 @@
 import tweepy
 
-auth = tweepy.OAuthHandler("FnMSfFgbEqZghDgGhjub5SXabRDdx6lgI8fu", "XKDwUzSEhgftYsjkAnkQcxsE6jt8qqsgfvhja4FR0Ufpmg21e6SfhfallPekIscizvFZv71")
-auth.set_access_token("832732350-UjPhjkkakljXQW59dfryAwkOkkAQ19K5tZvjFynvgjkagGjGjNkaklHavkDk6E9rPrwvDZ4VmMk", "yYxswg5yJH2YJmZg9ghkfawQxzhMVz67dgjklnEynEO3oXCwhgrMQBBjmq2hP7")
+auth = tweepy.OAuthHandler("aWqFnMSfFgbEqZghDgGhjub5SXabRDdx6lgI8fu", "XdgjKDwUzSEhgftYsjkAnkQcxsE6jt8qqsgfvhja4FR0Ufpmg21e6SfhfallPekIscizvFZv71")
+auth.set_access_token("13832732453680350-UasdjPhjkkakljXQW59dfryAwkOkkAQ19K5tZvjFynvgjkagGjGjNkaklHavkDk6E9rPrwvDZ4VmMk", "yYxsfadfhwg5yJH2YJmZg9ghkfawQxzhMVz67dgjklnEynEO3oXCwhgrMQBBjmq2hP7")
 
 api = tweepy.API(auth)
 public_tweets = api.home_timeline()
