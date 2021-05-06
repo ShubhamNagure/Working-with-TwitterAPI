704333daf1f6f378f559b0594dbbdccfb2e53bda
Shubham Nagure <49149160+ShubhamNagure@users.noreply.github.com>
2021-05-06 15:57:23 +0530

    sensitive data removed.

--- TwitAPIWork1.py
+++ TwitAPIWork1.py
@@ -1,7 +1,7 @@
 import tweepy
 
-auth = tweepy.OAuthHandler("aWqFnMSfFgbEqZghDgGhjub5SXabRDdx6lgI8fu", "XdgjKDwUzSEhgftYsjkAnkQcxsE6jt8qqsgfvhja4FR0Ufpmg21e6SfhfallPekIscizvFZv71")
-auth.set_access_token("13832732453680350-UasdjPhjkkakljXQW59dfryAwkOkkAQ19K5tZvjFynvgjkagGjGjNkaklHavkDk6E9rPrwvDZ4VmMk", "yYxsfadfhwg5yJH2YJmZg9ghkfawQxzhMVz67dgjklnEynEO3oXCwhgrMQBBjmq2hP7")
+auth = tweepy.OAuthHandler("--some key----", "---some more key----------")
+auth.set_access_token("----some token-------------", "------------some more token---")
 
 api = tweepy.API(auth)
 public_tweets = api.home_timeline()
